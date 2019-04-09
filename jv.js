959e8146a346a45dc8fc15891a1ba515656c5571
RENATA ALEXANDRE BIANCHI <1090481913027@fatec.local>
2019-04-08 21:27:50 -0300

    Alteraçõesnoite

---
+++ jv.js
@@ -0,0 +1,42 @@
+var cont = 1;
+function func(id) {
+    switch (cont) {
+        case 1:
+        document.getElementById(id).classList.add('x_id');
+        cont++;
+        break;
+        case 2:
+        document.getElementById(id).classList.add('certo_id');
+        cont++;
+        break;
+        case 3:
+        document.getElementById(id).classList.add('x_id');
+        cont++;
+        break;
+        case 4:
+        document.getElementById(id).classList.add('certo_id');
+        cont++;
+        break;
+        case 5:
+        document.getElementById(id).classList.add('x_id');
+        cont++;
+        break;
+        case 6:
+        document.getElementById(id).classList.add('certo_id');
+        cont++;
+        break;
+        case 7:
+        document.getElementById(id).classList.add('x_id');
+        cont++;
+        break;
+        case 8:
+        document.getElementById(id).classList.add('certo_id');
+        cont++;
+        break;
+        case 9:
+        document.getElementById(id).classList.add('x_id');
+        cont++;
+        break;
+        
+    }
+}
